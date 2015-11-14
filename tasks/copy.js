23e83a55c6507f4a57ac4b6fe1fb484e9d1e035f
yethon <ursula.yethon@mheducation.com>
2015-11-13 21:06:38 -0500

    Optimize assets

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,12 +1,21 @@
-var gulp        = require('gulp');
+var gulp       = require('gulp');
+var imagemin   = require('gulp-imagemin');
+var recompress = require('imagemin-jpeg-recompress');
 
 // copy static assets into the 'dist' folder
 gulp.task('copy-images', function () {
   return gulp.src('assets/img/**.*')
-    .pipe(gulp.dest('./dist/assets/img'));
+  .pipe(recompress({
+    loops: 10,
+    acurate: true,
+    quality: 'low',
+    target: 0.1,
+    max: 10
+  })())
+  .pipe(gulp.dest('./dist/assets/img'));
 });
 
 gulp.task('copy-index', function () {
   return gulp.src('index.html')
-    .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest('./dist'));
 });

39db25a4dd8df9c6ac5e262fa235bf01ca319e56
yethon <ursula_yethon@hotmail.com>
2016-11-15 17:56:59 -0500

    Site redesign.

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,17 +1,8 @@
 var gulp       = require('gulp');
-var imagemin   = require('gulp-imagemin');
-var recompress = require('imagemin-jpeg-recompress');
 
 // copy static assets into the 'dist' folder
 gulp.task('copy-images', function () {
   return gulp.src('assets/img/**.*')
-  .pipe(recompress({
-    loops: 10,
-    acurate: true,
-    quality: 'low',
-    target: 0.1,
-    max: 10
-  })())
   .pipe(gulp.dest('./dist/assets/img'));
 });
 
